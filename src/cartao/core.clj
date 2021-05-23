eb0e099296df3cd886168d37db8b2b2d1db0928e
Jaqueline Duque <jaqueline.duque@jaqueline.duque>
2021-05-23 11:22:40 -0300

    fixing data for test

--- src/cartao/core.clj
+++ src/cartao/core.clj
@@ -27,10 +27,10 @@
 
 ;adiciona compras no banco
 (def compra1 (c.model/nova-compra [:cartao/id (:cartao/id cartao-do-gandalf)] (clojure.instant/read-instant-date "2021-05-09T09:59:40.829") 50.01M "Padoca da Rosinha" :alimentacao))
-(def compra2 (c.model/nova-compra [:cartao/id (:cartao/id cartao-do-gandalf)] (clojure.instant/read-instant-date "2021-05-21T09:59:40.829") 28.97M "28.97" :lazer))
-(def compra3 (c.model/nova-compra [:cartao/id (:cartao/id cartao-do-alantar)] (clojure.instant/read-instant-date "2021-05-21T10:59:40.829") 10.97M "10.97" :lazer))
+(def compra2 (c.model/nova-compra [:cartao/id (:cartao/id cartao-do-gandalf)] (clojure.instant/read-instant-date "2021-05-21T09:59:40.829") 28.97M "Teatro dos magos" :lazer))
+(def compra3 (c.model/nova-compra [:cartao/id (:cartao/id cartao-do-alantar)] (clojure.instant/read-instant-date "2021-05-21T10:59:40.829") 10.97M "Teatro dos magos" :lazer))
 (c.db/adiciona-compras! conn [compra1 compra2 compra3])
-(c.db/adiciona-compras! conn [compra3])
+;(c.db/adiciona-compras! conn [compra3])
 ;retorna compras com todos os dados.
 (println "Todas as compras")
 (c.db/retorna-compras-completa (d/db conn))
